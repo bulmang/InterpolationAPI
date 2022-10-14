0874ddc80181c8fe8808682f0bc747b6555d5fab
Bulmang <hmk113737@gmail.com>
2022-10-14 23:27:07 +0900

    Initial Commit

---
+++ InterpolationAPI/InterpolationAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterpolationAPIApp.swift
+//  InterpolationAPI
+//
+//  Created by 하명관 on 2022/10/14.
+//
+
+import SwiftUI
+
+@main
+struct InterpolationAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
